30ab674079433be1d0d5d577c985d088449cc1c6
xDepcio <olek.drwal@gmail.com>
2023-08-26 11:39:19 +0200

    made toFormBody a helper function

--- app/api/leaderboards/guilds/[id]/deaths/route.ts
+++ app/api/leaderboards/guilds/[id]/deaths/route.ts
@@ -1,3 +1,4 @@
+import { toFormBody } from "@/lib/utils";
 import db from "@/lib/utils/prisma";
 import { NextResponse } from "next/server";
 
@@ -8,31 +9,20 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
 
     // const stats = await db.$queryRawUnsafe(`SELECT Deaths.id, Deaths.createdAt, Deaths.pointsLost, Players.nickname FROM Deaths JOIN Players ON Players.id = killerId WHERE killerId IN (SELECT Players.id FROM Players JOIN Guilds ON Players.guildId = Guilds.id WHERE guildId = ${guildId}) ORDER BY Deaths.createdAt DESC LIMIT 24;`)
 
-    let details: any = {
-        apikey: "2UUm4jwOJ3AudFqrZaaysOA6QOe",
-        dbowner: "xDepcio",
-        dbname: "dev.db",
-        sql: btoa(query)
-    };
-
-    let formBody: any = [];
-    for (let property in details) {
-        let encodedKey = encodeURIComponent(property);
-        let encodedValue = encodeURIComponent(details[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-    }
-    formBody = formBody.join("&");
-
     const res = await fetch('https://api.dbhub.io/v1/query', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
         },
-        body: formBody
+        body: toFormBody({
+            apikey: "2UUm4jwOJ3AudFqrZaaysOA6QOe",
+            dbowner: "xDepcio",
+            dbname: "dev.db",
+            sql: btoa(query)
+        })
     })
     const data = await res.json()
 
-    console.log(data)
     const formattedData = data.map((item: any) => {
         return {
             id: Number(item[0].Value),
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+/** changes any object to form data.
+Put 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' in headers
+*/
+export function toFormBody(details: any) {
+    let formBody: any = [];
+    for (let property in details) {
+        let encodedKey = encodeURIComponent(property);
+        let encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
+    }
+    return formBody.join("&");
 }
