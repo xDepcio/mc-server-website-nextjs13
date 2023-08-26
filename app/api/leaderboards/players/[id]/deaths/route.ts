cf6caa560f679539a50af1b21e301290d9b45844
xDepcio <olek.drwal@gmail.com>
2023-08-26 20:29:40 +0200

    changed player kills and deaths to serverless

--- app/api/leaderboards/players/[id]/deaths/route.ts
+++ app/api/leaderboards/players/[id]/deaths/route.ts
@@ -1,24 +1,39 @@
+import { cloudDb } from "@/lib/utils";
 import db from "@/lib/utils/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
     const playerId = Number(params.id)
 
-    const stats = await db.deaths.findMany({
-        where: {
-            deaderId: playerId
-        },
-        include: {
-            Players_Deaths_killerIdToPlayers: true,
-            // Players_Kills_killerIdToPlayers: true
-        },
-        take: 24,
-        orderBy: {
-            createdAt: "desc"
+    const deathsQuery = "SELECT `Death`.`id`, `Death`.`deaderId`, `Death`.`killerId`, `Death`.`pointsLost`, `Death`.`createdAt`, `Death`.`updatedAt`, `Player`.`id` AS `Player.id`, `Player`.`nickname` AS `Player.nickname`, `Player`.`points` AS `Player.points`, `Player`.`rank` AS `Player.rank` FROM `Deaths` AS `Death` LEFT OUTER JOIN `Players` AS `Player` ON `Death`.`killerId` = `Player`.`id` WHERE `Death`.`deaderId` = '" + `${playerId}` + "' ORDER BY `Death`.`createdAt` DESC LIMIT 24;"
+    const deaths = cloudDb.formatResponse(await cloudDb.query(deathsQuery))
+    const formattedDeaths = deaths.map((d: any) => ({
+        ...d,
+        Player: {
+            id: d["Player.id"],
+            nickname: d["Player.nickname"],
+            points: d["Player.points"],
+            rank: d["Player.rank"]
         }
-    })
+    }))
 
-    const changedStats = stats.map(s => ({ ...s, Player: s.Players_Deaths_killerIdToPlayers }))
+    return NextResponse.json(formattedDeaths)
 
-    return NextResponse.json(changedStats)
+    // const stats = await db.deaths.findMany({
+    //     where: {
+    //         deaderId: playerId
+    //     },
+    //     include: {
+    //         Players_Deaths_killerIdToPlayers: true,
+    //         // Players_Kills_killerIdToPlayers: true
+    //     },
+    //     take: 24,
+    //     orderBy: {
+    //         createdAt: "desc"
+    //     }
+    // })
+
+    // const changedStats = stats.map(s => ({ ...s, Player: s.Players_Deaths_killerIdToPlayers }))
+
+    // return NextResponse.json(changedStats)
 }
--- app/api/leaderboards/players/[id]/kills/route.ts
+++ app/api/leaderboards/players/[id]/kills/route.ts
@@ -1,24 +1,38 @@
+import { cloudDb } from "@/lib/utils";
 import db from "@/lib/utils/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
     const playerId = Number(params.id)
 
-    const stats = await db.kills.findMany({
-        where: {
-            killerId: playerId
-        },
-        include: {
-            Players_Kills_deaderIdToPlayers: true,
-            // Players_Kills_killerIdToPlayers: true
-        },
-        take: 24,
-        orderBy: {
-            createdAt: "desc"
+    const killsQuery = "SELECT `Kill`.`id`, `Kill`.`killerId`, `Kill`.`deaderId`, `Kill`.`pointsGained`, `Kill`.`createdAt`, `Kill`.`updatedAt`, `Player`.`id` AS `Player.id`, `Player`.`nickname` AS `Player.nickname`, `Player`.`points` AS `Player.points`, `Player`.`rank` AS `Player.rank` FROM `Kills` AS `Kill` LEFT OUTER JOIN `Players` AS `Player` ON `Kill`.`deaderId` = `Player`.`id` WHERE `Kill`.`killerId` = '" + `${playerId}` + "' ORDER BY `Kill`.`createdAt` DESC LIMIT 24;"
+    const kills = cloudDb.formatResponse(await cloudDb.query(killsQuery))
+    const formttedKills = kills.map((k: any) => ({
+        ...k,
+        Player: {
+            id: k["Player.id"],
+            nickname: k["Player.nickname"],
+            points: k["Player.points"],
+            rank: k["Player.rank"]
         }
-    })
+    }))
 
-    const changedStats = stats.map(s => ({ ...s, Player: s.Players_Kills_deaderIdToPlayers }))
+    return NextResponse.json(formttedKills)
+    // const stats = await db.kills.findMany({
+    //     where: {
+    //         killerId: playerId
+    //     },
+    //     include: {
+    //         Players_Kills_deaderIdToPlayers: true,
+    //         // Players_Kills_killerIdToPlayers: true
+    //     },
+    //     take: 24,
+    //     orderBy: {
+    //         createdAt: "desc"
+    //     }
+    // })
 
-    return NextResponse.json(changedStats)
+    // const changedStats = stats.map(s => ({ ...s, Player: s.Players_Kills_deaderIdToPlayers }))
+
+    // return NextResponse.json(changedStats)
 }
