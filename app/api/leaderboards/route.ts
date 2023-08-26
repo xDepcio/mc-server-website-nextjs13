fc9f39030319c56d5f1f65fd46b84a195f30e72c
xDepcio <olek.drwal@gmail.com>
2023-08-26 13:16:05 +0200

    added main leaderboards reoute as serverless

--- app/api/leaderboards/route.ts
+++ app/api/leaderboards/route.ts
@@ -1,75 +1,127 @@
+import { cloudDb } from "@/lib/utils"
 import db from "@/lib/utils/prisma"
 import { NextResponse } from "next/server"
+import { inspect } from 'util'
 
 export const GET = async (req: Request) => {
     const { searchParams } = new URL(req.url)
     const limit = Number(searchParams.get("limit")) || 10
     const offset = Number(searchParams.get("offset")) || 0
 
-    const players = await db.players.findMany({
-        orderBy: {
-            points: "desc"
-        },
-        take: limit,
-        skip: offset,
-    })
+    // const players = await db.players.findMany({
+    //     orderBy: {
+    //         points: "desc"
+    //     },
+    //     take: limit,
+    //     skip: offset,
+    // })
+    const playersC = cloudDb.formatResponse(await cloudDb.query("SELECT `main`.`Players`.`id`, `main`.`Players`.`nickname`, `main`.`Players`.`points`, `main`.`Players`.`guildId`, `main`.`Players`.`rank`, `main`.`Players`.`guildRank` FROM `main`.`Players` WHERE 1=1 ORDER BY `main`.`Players`.`points` DESC LIMIT " + `${limit}` + " OFFSET " + `${offset}` + ""))
+    console.log(playersC)
 
-    const playerCount = await db.players.count()
-
-    const guildsGroup = await db.players.groupBy({
-        by: ["guildId"],
-        _avg: {
-            points: true
-        },
-        orderBy: {
-            _avg: {
-                points: "desc"
-            }
-        },
-        where: {
-            guildId: {
-                not: null
-            }
-        },
-        take: limit,
-        skip: offset,
-    })
-
-    const guildsIds = guildsGroup.map(g => g.guildId!)
-
-    const guilds = await db.guilds.findMany({
-        include: {
-            Players: {
-                where: {
-                    guildRank: {
-                        equals: "leader"
-                    }
-                }
-            }
-        },
-        where: {
-            id: {
-                in: guildsIds
-            }
+    // const guildsGroup = await db.players.groupBy({
+    //     by: ["guildId"],
+    //     _avg: {
+    //         points: true
+    //     },
+    //     orderBy: {
+    //         _avg: {
+    //             points: "desc"
+    //         }
+    //     },
+    //     where: {
+    //         guildId: {
+    //             not: null
+    //         }
+    //     },
+    //     take: limit,
+    //     skip: offset,
+    // })
+    const guildsGroupC = cloudDb.formatResponse(
+        await cloudDb.query("SELECT AVG(`main`.`Players`.`points`), `main`.`Players`.`guildId` FROM `main`.`Players` WHERE `main`.`Players`.`guildId` IS NOT NULL GROUP BY `main`.`Players`.`guildId` ORDER BY AVG(`main`.`Players`.`points`) DESC LIMIT " + `${limit}` + " OFFSET " + `${offset}` + ""),
+        {
+            fieldName: "AVG(`main`.`Players`.`points`)",
+            newFieldName: "_avg",
+            replaceWith: (entry) => ({
+                points: Number(entry.Value)
+            })
         }
-    })
+    )
+    console.log(guildsGroupC)
 
-    const guildsFinal = guildsGroup.map(g => {
-        const guild = guilds.find(gg => gg.id === g.guildId)
+    // const guildsIds = guildsGroup.map(g => g.guildId!)
+    const guildsIds = guildsGroupC.map((g: any) => g.guildId!)
 
+    // const guilds = await db.guilds.findMany({
+    //     include: {
+    //         Players: {
+    //             where: {
+    //                 guildRank: {
+    //                     equals: "leader"
+    //                 }
+    //             }
+    //         }
+    //     },
+    //     where: {
+    //         id: {
+    //             in: guildsIds
+    //         }
+    //     }
+    // })
+    const query1 = "SELECT `main`.`Guilds`.`id`, `main`.`Guilds`.`name`, `main`.`Guilds`.`createdAt`, `main`.`Guilds`.`updatedAt` FROM `main`.`Guilds` WHERE `main`.`Guilds`.`id` IN (" + `${guildsIds.join(',')}` + ") LIMIT -1 OFFSET 0"
+    const guildsC1 = cloudDb.formatResponse(
+        await cloudDb.query(query1)
+    )
+
+    const query2 = "SELECT `main`.`Players`.`id`, `main`.`Players`.`nickname`, `main`.`Players`.`points`, `main`.`Players`.`guildId`, `main`.`Players`.`rank`, `main`.`Players`.`guildRank` FROM `main`.`Players` WHERE (`main`.`Players`.`guildRank` = " + `"leader"` + " AND `main`.`Players`.`guildId` IN (" + `${guildsIds.join(',')}` + ")) LIMIT " + `${-1}` + " OFFSET " + `${0}` + ""
+    const guildsC2 = cloudDb.formatResponse(
+        await cloudDb.query(query2)
+    )
+    // console.log(guildsC1)
+    // console.log(guildsC2)
+    // console.log(inspect(guilds, {
+    //     depth: null
+    // }))
+
+    const formattedGuildsC = guildsC1.map((g: any) => {
+        const player = guildsC2.find((gg: any) => gg.guildId === g.id)
+        // console.log(g)
         return {
-            ...guild?.Players[0],
-            avgPoints: g._avg.points,
+            ...player,
+            avgPoints: guildsGroupC.find((gd: any) => gd.guildId === g.id)._avg.points,
             Guild: {
-                ...guild
+                ...g
             }
         }
-    })
+    }).sort((a: any, b: any) => b.avgPoints - a.avgPoints)
+    console.log(formattedGuildsC)
+    // console.log(inspect(formattedGuildsC, { depth: null }), 'dasdsa')
 
+    // const guildsFinal = guildsGroup.map(g => {
+    //     const guild = guilds.find(gg => gg.id === g.guildId)
+
+    //     return {
+    //         ...guild?.Players[0],
+    //         avgPoints: g._avg.points,
+    //         Guild: {
+    //             ...guild
+    //         }
+    //     }
+    // })
+
+    let totalPlayers = await cloudDb.query("SELECT COUNT(*) FROM (SELECT `main`.`Players`.`id` FROM `main`.`Players` WHERE 1=1 LIMIT -1 OFFSET 0) AS `sub`")
+    totalPlayers = Number(totalPlayers[0][0].Value)
+    // console.log(totalPlayers)
+    let totalGuilds = await cloudDb.query("SELECT COUNT(*) FROM (SELECT `main`.`Guilds`.`id` FROM `main`.`Guilds` WHERE 1=1 LIMIT -1 OFFSET 0) AS `sub`")
+    totalGuilds = Number(totalGuilds[0][0].Value)
+    // console.log(totalGuilds)
     return NextResponse.json({
-        totalPlayers: await db.players.count(),
-        totalGuilds: await db.guilds.count(),
-        players,
-        guilds: guildsFinal
+        // totalPlayers: await db.players.count(),
+        totalPlayers: totalPlayers,
+        // totalGuilds: await db.guilds.count(),
+        totalGuilds: totalGuilds,
+        // players,
+        players: playersC,
+        // guilds: guildsFinal
+        guilds: formattedGuildsC
     })
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -17,3 +17,47 @@ export function toFormBody(details: any) {
     }
     return formBody.join("&");
 }
+
+export const cloudDb = {
+    apikey: "2UUm4jwOJ3AudFqrZaaysOA6QOe",
+    dbowner: "xDepcio",
+    dbname: "dev.db",
+    query: async (query: string) => {
+        const res = await fetch('https://api.dbhub.io/v1/query', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: toFormBody({
+                apikey: cloudDb.apikey,
+                dbowner: cloudDb.dbowner,
+                dbname: cloudDb.dbname,
+                sql: btoa(query)
+            })
+        })
+        const data = await res.json()
+        return data
+    },
+    formatResponse: (data: any, replaceField?: { fieldName: string, newFieldName: string, replaceWith: (entry: any) => any }) => {
+        const formattedData = data.map((item: any) => {
+            // console.log(item)
+            const formattedItem: any = item.reduce((acc: any, curr: any) => {
+                let value = curr.Type === 4 ? Number(curr.Value) : curr.Value
+
+                let name: string = curr.Name
+                if (replaceField) {
+                    if (curr.Name === replaceField.fieldName) {
+                        name = replaceField.newFieldName
+                        acc[name] = replaceField.replaceWith(curr)
+                        return acc
+                    }
+                }
+
+                acc[name] = value
+                return acc
+            }, {})
+            return formattedItem
+        })
+        return formattedData
+    }
+}
