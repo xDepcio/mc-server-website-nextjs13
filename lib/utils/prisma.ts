fd2d170e70371fdaff575b0f3a35d2554117ca39
xDepcio <olek.drwal@gmail.com>
2023-08-26 11:51:38 +0200

    added prisma logs

--- lib/utils/prisma.ts
+++ lib/utils/prisma.ts
@@ -1,5 +1,32 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient()
+const db = new PrismaClient({
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'stdout',
+            level: 'error',
+        },
+        {
+            emit: 'stdout',
+            level: 'info',
+        },
+        {
+            emit: 'stdout',
+            level: 'warn',
+        },
+    ],
+})
+
+db.$on('query', (e) => {
+    console.log('============> Query <============')
+    console.log('Query: ' + e.query)
+    console.log('Params: ' + e.params)
+    console.log('Duration: ' + e.duration + 'ms')
+    console.log('=================================')
+})
 
 export default db
